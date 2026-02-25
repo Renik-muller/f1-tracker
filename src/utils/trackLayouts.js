52f07b2fdbd2fe5f4f3ea634119d25698a0a1467
Renik Muller <reniksione@hotmail.com>
2026-02-25 16:39:00 -0500

    fix: switch track layout URLs to Wikimedia Commons (CORS fix)

--- src/utils/trackLayouts.js
+++ src/utils/trackLayouts.js
@@ -1,39 +1,38 @@
-// Circuit ID → track layout SVG URL (from bacinger/f1-circuits on GitHub)
-// Each key matches the circuit's Ergast/Jolpica circuitId
-const BASE = 'https://raw.githubusercontent.com/bacinger/f1-circuits/master/circuits';
+// Circuit SVG layouts from Wikimedia Commons (reliable, embeddable)
+// Using PNG thumbnails from Wikimedia for consistent loading
+const WM = 'https://upload.wikimedia.org/wikipedia/commons/thumb';
 
 export const TRACK_LAYOUTS = {
-    albert_park: `${BASE}/au-2011.svg`,
-    shanghai: `${BASE}/cn-2004.svg`,
-    suzuka: `${BASE}/jp-1987.svg`,
-    bahrain: `${BASE}/bh-2004.svg`,
-    jeddah: `${BASE}/sa-2021.svg`,
-    miami: `${BASE}/us-2022.svg`,
-    imola: `${BASE}/it-2020.svg`,
-    monaco: `${BASE}/mc-1966.svg`,
-    villeneuve: `${BASE}/ca-1978.svg`,
-    catalunya: `${BASE}/es-1991.svg`,
-    red_bull_ring: `${BASE}/at-1997.svg`,
-    silverstone: `${BASE}/gb-1987.svg`,
-    hungaroring: `${BASE}/hu-1986.svg`,
-    spa: `${BASE}/be-1979.svg`,
-    zandvoort: `${BASE}/nl-2020.svg`,
-    monza: `${BASE}/it-1950.svg`,
-    baku: `${BASE}/az-2016.svg`,
-    marina_bay: `${BASE}/sg-2008.svg`,
-    americas: `${BASE}/us-2012.svg`,
-    rodriguez: `${BASE}/mx-1962.svg`,
-    interlagos: `${BASE}/br-1984.svg`,
-    las_vegas: `${BASE}/us-2023.svg`,
-    losail: `${BASE}/qa-2004.svg`,
-    yas_marina: `${BASE}/ae-2009.svg`,
+    albert_park: `${WM}/8/8e/Albert_Park_circuit_map.svg/400px-Albert_Park_circuit_map.svg.png`,
+    shanghai: `${WM}/6/6e/Shanghai_Circuit_labeled.svg/400px-Shanghai_Circuit_labeled.svg.png`,
+    suzuka: `${WM}/1/1a/Suzuka_circuit_map.svg/400px-Suzuka_circuit_map.svg.png`,
+    bahrain: `${WM}/1/10/Bahrain_International_Circuit--Sakhir--map.svg/400px-Bahrain_International_Circuit--Sakhir--map.svg.png`,
+    jeddah: `${WM}/3/3d/Jeddah_Street_Circuit_2021.svg/400px-Jeddah_Street_Circuit_2021.svg.png`,
+    miami: `${WM}/5/58/Miami_International_Autodrome_map.svg/400px-Miami_International_Autodrome_map.svg.png`,
+    imola: `${WM}/5/5d/Autodromo_Enzo_e_Dino_Ferrari_map.svg/400px-Autodromo_Enzo_e_Dino_Ferrari_map.svg.png`,
+    monaco: `${WM}/5/52/Monte_Carlo_Formula_1_track_map.svg/400px-Monte_Carlo_Formula_1_track_map.svg.png`,
+    villeneuve: `${WM}/a/a0/Circuit_Gilles_Villeneuve_track_map.svg/400px-Circuit_Gilles_Villeneuve_track_map.svg.png`,
+    catalunya: `${WM}/1/13/Circuit_de_Barcelona-Catalunya_track_map.svg/400px-Circuit_de_Barcelona-Catalunya_track_map.svg.png`,
+    red_bull_ring: `${WM}/7/7e/Red_Bull_Ring_track_map.svg/400px-Red_Bull_Ring_track_map.svg.png`,
+    silverstone: `${WM}/7/71/Silverstone_Circuit_2010.svg/400px-Silverstone_Circuit_2010.svg.png`,
+    hungaroring: `${WM}/a/a3/Hungaroring_track_map.svg/400px-Hungaroring_track_map.svg.png`,
+    spa: `${WM}/b/b8/Spa-Francorchamps_track_map.svg/400px-Spa-Francorchamps_track_map.svg.png`,
+    zandvoort: `${WM}/3/37/Circuit_Zandvoort_track_map.svg/400px-Circuit_Zandvoort_track_map.svg.png`,
+    monza: `${WM}/b/bb/Monza_track_map.svg/400px-Monza_track_map.svg.png`,
+    baku: `${WM}/6/6e/Baku_Formula_One_circuit_2016.svg/400px-Baku_Formula_One_circuit_2016.svg.png`,
+    marina_bay: `${WM}/e/e5/Singapore_street_circuit_2012.svg/400px-Singapore_street_circuit_2012.svg.png`,
+    americas: `${WM}/5/51/Austin_circuit_of_the_americas.svg/400px-Austin_circuit_of_the_americas.svg.png`,
+    rodriguez: `${WM}/4/44/Mexico_City_circuit_map.svg/400px-Mexico_City_circuit_map.svg.png`,
+    interlagos: `${WM}/a/a8/Autódromo_José_Carlos_Pace_track_map.svg/400px-Autódromo_José_Carlos_Pace_track_map.svg.png`,
+    las_vegas: `${WM}/1/1a/Las_Vegas_Street_Circuit_2023.svg/400px-Las_Vegas_Street_Circuit_2023.svg.png`,
+    losail: `${WM}/7/7a/Losail_International_Circuit_track_map.svg/400px-Losail_International_Circuit_track_map.svg.png`,
+    yas_marina: `${WM}/9/9a/Yas_Marina_Circuit_2021_F1_weekend_map.svg/400px-Yas_Marina_Circuit_2021_F1_weekend_map.svg.png`,
 };
 
 export function getTrackLayout(circuitId) {
     return TRACK_LAYOUTS[circuitId] || null;
 }
 
-// Extra circuit info beyond what the API returns
 export const CIRCUIT_INFO = {
     albert_park: { laps: 58, length: '5.278 km', turns: 16 },
     shanghai: { laps: 56, length: '5.451 km', turns: 16 },
